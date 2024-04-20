88330b7530587ed20936f6e3fa713d3fea5b808a
Yash verma <yv48183@gmail.com>
2024-04-20 06:44:35 +0530

    Initial Commit

---
+++ SwiftUi-weather/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUi-weather
+//
+//  Created by YASH VERMA on 20/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUi-weather/SwiftUi_weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUi_weatherApp.swift
+//  SwiftUi-weather
+//
+//  Created by YASH VERMA on 20/04/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUi_weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
